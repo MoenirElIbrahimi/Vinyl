5eb67e91dc7a0fc5b8f39fa49b037148beb57815
merijnkoppes <93912577+merijnkoppes@users.noreply.github.com>
2024-12-02 10:46:11 +0100

    Muziekquiz

---
+++ muziekquiz.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Home | Vinyl</title>
+    <link href="https://fonts.googleapis.com/css?family=Inter&display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet" />
+    <link href="./css/muziekquiz.css" rel="stylesheet" />
+    <link rel="shortcut" href="#" />
+  </head>
+  <body>
+    <section>
+    <div class="v5_670">
+        <div class="v2007_101"></div>
+        <div class="v2012_126"></div>
+        <div class="v2007_44"></div>
+        <div class="v2007_9"></div>
+        <div class="v2007_10"></div><span class="v2007_13">Home</span><span class="v2007_14">Home</span>
+        <div class="name"></div>
+        <div class="name"></div>
+        <div class="name"></div><span class="v2007_24">Vul de vragenlijst in zodat wij je nieuws en aanbevelingen kunnen
+            geven op basis van je muzieksmaak!</span><span class="v2007_33">Met welke genre(s) associeer jij jezelf het
+            meest?</span>
+        <div class="v2007_41"></div><span class="v2007_43">HipHop</span>
+        <div class="v2007_45"></div>
+        <div class="v2007_53"></div><span class="v2007_47">Rock</span>
+        <div class="v2007_55"></div>
+        <div class="v2007_60"></div><span class="v2007_57">Pop</span>
+        <div class="v2007_62"></div>
+        <div class="v2007_67"></div><span class="v2007_64">Klassiek</span>
+        <div class="v2007_79"></div>
+        <div class="v2007_84"></div><span class="v2007_81">Reggae</span>
+        <div class="v2007_86"></div>
+        <div class="v2007_91"></div><span class="v2007_88">Edm</span>
+        <div class="v2007_94"></div>
+        <div class="v2007_99"></div><span class="v2007_96">Country</span>
+        <div class="v2012_103"></div><span class="v2012_104">Naar welk van deze artiesten luister jij het liefst?</span>
+        <div class="v2012_129"></div><span class="v2012_106">Linkin Park</span>
+        <div class="v2012_107"></div>
+        <div class="v2012_132"></div><span class="v2012_109">Billie Eilish</span>
+        <div class="v2012_110"></div>
+        <div class="v2012_135"></div><span class="v2012_112">Eminem</span>
+        <div class="v2012_113"></div>
+        <div class="v2012_138"></div><span class="v2012_115">Beethoven</span>
+        <div class="v2012_116"></div>
+        <div class="v2012_141"></div><span class="v2012_118">Ariana grande</span>
+        <div class="v2012_119"></div>
+        <div class="v2012_144"></div><span class="v2012_121">Martin garrix</span>
+        <div class="v2012_122"></div>
+        <div class="v2012_147"></div><span class="v2012_124">P diddy</span>
+    </div>
+    </section>
+  </body>
+</html>
